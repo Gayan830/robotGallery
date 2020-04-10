b5d2388504c1b401bb176db59f98d4bd491a3018
Gayan830 <malshan95104@gmail.com>
2020-04-10 11:59:01 +0530

    finish cards creation.

--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class Card extends Component {
-    render() {
-        return (
-            <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-                <img alt='robot' src='https://robohash.org/test?200*200' />
-                <div>
-                    <h2>Jane</h2>
-                    <p>Jane@gmail.com</p>
-                </div>
+const Card = ({name, email, id}) =>{
+    return  (
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img alt='robot' src={`https://robohash.org/${id}/test?200*200`} />
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
-        )
-    }
+        </div>
+    )    
 }
-
 export default Card
