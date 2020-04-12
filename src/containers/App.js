9cc55908caeee38f22dc0750e458058f92ea92aa
Gayan830 <malshan95104@gmail.com>
2020-04-12 10:27:29 +0530

    finalize robots...

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React,{Component} from 'react'
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
-import Scroll from '../components/Scroll' 
+import Scroll from '../components/Scroll'
+import ErrorBoundry from '../components/ErrorBoundry' 
 import './app.css'
 
 class App extends Component {
@@ -38,7 +39,9 @@ class App extends Component {
                     <h1 className='f1'>RobotFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
                     <Scroll>
-                        <CardList robots={filteredRobots}/>
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots}/>
+                        </ErrorBoundry>
                     </Scroll>
                 </div>
         )
